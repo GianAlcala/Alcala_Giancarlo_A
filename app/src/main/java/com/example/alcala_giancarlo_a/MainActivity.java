a46e89167f209123ed090c07abf18bf08587a0eb
GianAlcala <giancarlovitorio.alcala.cics@ust.edu.ph>
2022-11-19 11:48:04 +0800

    Saved SP

--- app/src/main/java/com/example/alcala_giancarlo_a/MainActivity.java
+++ app/src/main/java/com/example/alcala_giancarlo_a/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.alcala_giancarlo_a;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button sp1BtnCall = (Button)findViewById(R.id.SP1); //button listener
+        sp1BtnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences sp = getSharedPreferences("User Preferences", Context.MODE_PRIVATE);
+                SharedPreferences.Editor edit = sp.edit();
+                edit.putString("Task", "Saving Data");
+                edit.putString("Location", "SharedPreferences");
+            }
+        });
     }
+
+
+
+
+
 }
